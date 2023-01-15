262abd9ecf5cb9abd169cc58a02672bcf6d6f15c
Beni Permadani <94419695+benipermadani@users.noreply.github.com>
2023-01-15 21:49:02 +0800

    deklarasi variable dan tipe data

---
+++ Modul01/DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        
+        String namaDepan = "Beni";
+        String namaTengah = " ";
+        String namaBelakang = "Permadani";
+        int usia = 18;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89764512;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+    }
+}
